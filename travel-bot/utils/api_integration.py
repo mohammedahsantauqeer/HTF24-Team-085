96e8d613692214a0b0a69f83abebaf9fbc3120b4
mohammedahsantauqeer <ahsanashlad2011@gmail.com>
2024-10-26 23:21:55 +0530

    Update api_integration.py

--- travel-bot/utils/api_integration.py
+++ travel-bot/utils/api_integration.py
@@ -1,1 +1,10 @@
+import requests
+import os
+
+OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY')
+
+def get_weather(location):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={OPENWEATHER_API_KEY}&units=metric"
+    response = requests.get(url)
+    return response.json()
 
